c0cd3cc32af40b09c7d0915612b8f34c8c2bc7a8
isolesty23 <isolesty0814@gmail.com>
2021-07-22 00:46:01 +0900

    move, jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,10 +22,31 @@ public class PlayerController : MonoBehaviour
     }
 
     [Serializable]
+    public class Value
+    {
+        public Vector3 moveVector;
+        public Vector3 moveVelocity;
+        public float velocityY;
+        public float gravity = 1f;
+        public Vector3 groundNormal;
+        public float groundedDistance = 0f;
+        public float groundedCheck = 2f;
+        public float forwardCheck = 0.1f;
+    }
+
+    [Serializable]
     public class PlayerState
     {
         public bool isMoving;
         public bool isGrounded;
+        public bool isJumping;
+        public bool isForwardBlocked;
+    }
+
+    [Serializable]
+    public class InputValue
+    {
+        public float movementInput = 0;
     }
 
     [Serializable]
@@ -44,15 +65,20 @@ public class PlayerController : MonoBehaviour
     #region
     [SerializeField] private Components components = new Components();
     [SerializeField] private PlayerStatus playerStatus = new PlayerStatus();
+    [SerializeField] private Value value = new Value();
+    [SerializeField] private InputValue input = new InputValue();
     [SerializeField] private PlayerState playerState = new PlayerState();
     [SerializeField] private KeyOption keyOption = new KeyOption();
 
     public Components Com => components;
     public PlayerStatus Stat => playerStatus;
+    public Value Val => value;
+    public InputValue InputVal => input;
     public PlayerState State => playerState;
     public KeyOption Key => keyOption;
 
-    private Vector2 moveDir;
+    private Vector3 capsulePoint1 => new Vector3(transform.position.x, Com.collider.radius, 0);
+    private Vector3 capsulePoint2 => new Vector3(transform.position.x, transform.position.y + Com.collider.height/2 - Com.collider.radius, 0);
 
 
     #endregion
@@ -65,37 +91,48 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         SetInput();
-        GroundCheck();
-
-        Move();
-        Jump();
     }
 
     private void FixedUpdate()
     {
+        GroundCheck();
+        ForwardCheck();
+        UpdatePhysics();
+
+        Jump();
+        Move();
     }
 
     private void SetInput()
     {
-        float movementInput =0;
-        if (Input.GetKey(Key.moveLeft)) movementInput = -1f;
-        if (Input.GetKey(Key.moveRight)) movementInput = 1f;
-
-        moveDir.x = movementInput;
-
-        State.isMoving = moveDir.x != 0;
+        InputVal.movementInput = 0f;
+        if (Input.GetKey(Key.moveLeft)) InputVal.movementInput = -1f;
+        if (Input.GetKey(Key.moveRight)) InputVal.movementInput = 1f;
     }
 
     private void Move()
     {
-        if(State.isMoving == false)
+        Val.moveVector = new Vector3(InputVal.movementInput, 0, 0);
+        State.isMoving = Val.moveVector.x != 0;
+
+        //slope vector
+        if(State.isGrounded || Val.groundedDistance < Val.groundedCheck && !State.isJumping)
         {
-            Com.rigidbody.velocity = new Vector3(0, Com.rigidbody.velocity.y, 0);
-            return;
+            Vector3 projection = Vector3.ProjectOnPlane(Val.moveVector, Val.groundNormal);
+            Val.moveVector = new Vector3(projection.x, projection.y, 0).normalized;
         }
 
-        float moveVector = moveDir.x * Stat.movementSpeed;
-        Com.rigidbody.velocity = new Vector3(moveVector, Com.rigidbody.velocity.y, 0);
+        //forward block
+        if (State.isForwardBlocked && !State.isGrounded || State.isJumping && State.isGrounded)
+        {
+            Val.moveVelocity = Vector3.zero;
+        }
+        else
+        {
+            Val.moveVelocity = Val.moveVector * Stat.movementSpeed;
+        }
+
+        Com.rigidbody.velocity = new Vector3(Val.moveVelocity.x, Val.moveVelocity.y, 0) + (Vector3.up * Val.velocityY);
     }
 
     private void GroundCheck()
@@ -106,8 +143,10 @@ public class PlayerController : MonoBehaviour
         const float threshold = 0.01f;
 
         bool cast = Physics.SphereCast(transform.position, Com.collider.radius, Vector3.down, out var hit, rayDistance, layerMask);
-        float distance = cast ? hit.distance + Com.collider.radius - Com.collider.height / 2 : rayDistance;
-        State.isGrounded = distance <= threshold;
+        Val.groundedDistance = cast ? hit.distance + Com.collider.radius - Com.collider.height / 2 : rayDistance;
+        State.isGrounded = Val.groundedDistance <= threshold;
+
+        Val.groundNormal = hit.normal;
     }
 
     private void Jump()
@@ -115,9 +154,35 @@ public class PlayerController : MonoBehaviour
         if (!State.isGrounded)
             return;
 
-        if(Input.GetKeyDown(Key.jump))
+        if(Input.GetKey(Key.jump))
         {
-            Com.rigidbody.AddForce(Vector3.up * Stat.jumpForce, ForceMode.VelocityChange);
+            Val.velocityY = Stat.jumpForce;
+            State.isJumping = true;
+        }
+    }
+
+    private void UpdatePhysics()
+    {
+        //gravity
+        if(State.isGrounded)
+        {
+            Val.velocityY = 0f;
+            State.isJumping = false;
+        }
+        else
+        {
+            Val.velocityY -= Val.gravity * Time.fixedDeltaTime;
+        }
+    }
+
+    private void ForwardCheck()
+    {
+        State.isForwardBlocked = false;
+
+        if (Physics.CapsuleCast(capsulePoint1, capsulePoint2, Com.collider.radius * 0.8f, Vector3.right * input.movementInput, Val.forwardCheck, -1, QueryTriggerInteraction.Ignore))
+        {
+            Debug.Log("blocked");
+            State.isForwardBlocked = true;
         }
     }
 }
