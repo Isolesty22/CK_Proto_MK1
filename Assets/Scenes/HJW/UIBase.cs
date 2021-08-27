991612b3e5200d57237b4f115a4c562e4e7810be
JiWonHan <acsang12@gmail.com>
2021-08-27 20:43:27 +0900

    - HJW 폴더 생성 - UIBase, UIManager 추가

---
+++ Assets/Scenes/HJW/UIBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UI들의 상위 클래스 
+/// </summary>
+public class UIBase : MonoBehaviour
+{
+   
+}
---
+++ Assets/Scenes/HJW/UIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UI를 작동/관리하는 매니저 클래스
+/// </summary>
+
+public class UIManager : MonoBehaviour
+{
+
+}
