b935b95e3d2978715cd16fc853f5c0156b1003d2
JiWonHan <acsang12@gmail.com>
2021-08-27 22:13:49 +0900

    UIManager에 uiList추가, UIBase 기초 작업

--- Assets/Scripts/UI/UIBase.cs
+++ Assets/Scripts/UI/UIBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,5 +8,37 @@ using UnityEngine;
 /// </summary>
 public class UIBase : MonoBehaviour
 {
-   
+
+    #region definitions
+    /// <summary>
+    /// UIBase의 컴포넌트 모음.
+    /// </summary>
+    [Serializable]
+    public class Components
+    {
+        public Canvas canvas;
+        public CanvasGroup canvasGroup;
+    }
+    #endregion
+
+    #region field
+
+    [SerializeField] private Components components = new Components();
+
+    public Components Com => components;
+
+    #endregion
+
+
+    public virtual void Open() { }
+    public virtual void Close() { }
+
+    /// <summary>
+    /// UIManager에 해당 UI를 등록합니다.
+    /// </summary>
+    public virtual void RegisterUIManager()
+    {
+        UIManager.Instance.RegisterThis(this);
+    }
+
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,4 +9,44 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
 
+    #region Instance
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    private List<UIBase> uiList = new List<UIBase>();
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            instance = this;
+
+            // DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Debug.Log("이미 instance가 존재합니다." + this);
+        }
+    }
+
+
+    /// <summary>
+    /// uiList에 해당 uiBase를 Add합니다.
+    /// </summary>
+    public void RegisterThis(UIBase _uiBase)
+    {
+        uiList.Add(_uiBase);
+    }
 }
