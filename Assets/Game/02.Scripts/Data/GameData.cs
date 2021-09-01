cff9302d467605a07ce2f121a21a66bd810a6efa
JiwonHan <acsang12@gmail.com>
2021-09-01 18:37:49 +0900

    세팅데이터, 플레이어데이터 기본 틀 생성

---
+++ Assets/Game/02.Scripts/Data/GameData.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData
+{
+
+}
+
+[System.Serializable]
+public class Data_Settings : GameData
+{
+    //볼륨값
+
+    public string volume_master;
+    public string volume_sfx;
+    public string volume_bgm;
+
+
+    //음소거 상태
+
+    public bool isMute_master;
+    public bool isMute_sfx;
+    public bool isMute_bgm;
+
+    //해상도
+    public eResolutionType resolutionType;
+
+
+    /// <summary>
+    /// 디폴트 생성자
+    /// </summary>
+    public Data_Settings()
+    {
+        volume_master = "0.7";
+        volume_bgm = "0.5";
+        volume_sfx = "0.5";
+
+        isMute_master = false;
+        isMute_bgm = false;
+        isMute_sfx = false;
+
+        resolutionType = eResolutionType.FHD;
+    }
+
+
+
+    public Data_Settings(Data_Settings _data)
+    {
+        CopyData(_data);
+    }
+
+    /// <summary>
+    /// 현재 데이터를 해당 데이터로 설정합니다.
+    /// </summary>
+    public void CopyData(Data_Settings _data)
+    {
+        volume_master = _data.volume_master;
+        volume_bgm = _data.volume_bgm;
+        volume_sfx = _data.volume_sfx;
+
+        isMute_master = _data.isMute_master;
+        isMute_bgm = _data.isMute_bgm;
+        isMute_sfx = _data.isMute_sfx;
+
+        resolutionType = eResolutionType.FHD;
+    }
+
+
+}
+
+
+[System.Serializable]
+public class Data_Player
+{
+    /// <summary>
+    /// 현재 스테이지 이름(씬이름?)
+    /// </summary>
+    public string currentStageName;
+
+    /// <summary>
+     /// 디폴트 생성자
+     /// </summary>
+    public Data_Player()
+    {
+
+    }
+
+    public Data_Player(Data_Player _data)
+    {
+
+    }
+
+    /// <summary>
+    /// 현재 데이터를 해당 데이터로 설정합니다.
+    /// </summary>
+    public void CopyData(Data_Player _data)
+    {
+
+    }
+}
---
+++ Assets/Game/02.Scripts/ETC/EnumCollection.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum eResolutionType
+{
+    /// <summary>
+    /// 1280x720
+    /// </summary>
+    HD,
+
+    /// <summary>
+    /// 1920x1080
+    /// </summary>
+    FHD,
+
+    /// <summary>
+    /// 3840x2160
+    /// </summary>
+    UHD,
+}
+
+
+public class EnumCollection : MonoBehaviour
+{
+
+}
