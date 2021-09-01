f7a63cb37784bf255ca89bcd9eb0594f62456122
JiwonHan <acsang12@gmail.com>
2021-09-01 20:24:05 +0900

    FileManager, FileBase 추가

---
+++ Assets/Game/02.Scripts/Data/FileBase.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileBase
+{
+    public virtual IEnumerator WriteText(string _dataName, string _data, string _path)
+    {
+        yield break;
+    }
+
+    public string readText_Result;
+    public virtual IEnumerator ReadText(string _dataName, string _path)
+    {
+        yield break;
+    }
+
+    /// <summary>
+    /// Application.dataPath를 사용해서 경로를 가져옵니다.
+    /// </summary>
+    public virtual string GetDataLocation_DataPath()
+    {
+        return string.Empty;
+    }
+
+    /// <summary>
+    /// IsExist의 결과값
+    /// </summary>
+    public bool isExist_result;
+    public virtual IEnumerator IsExist(string dataName, string dataPath)
+    {
+        yield break;
+    }
+
+}
---
+++ Assets/Game/02.Scripts/Data/FileManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileManager : MonoBehaviour
+{
+    private FileBase fileBase;
+
+    private void Awake()
+    {
+        #region legacy
+        //#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+
+        //        fileBase = new FileBaseWindows();
+        //#endif
+
+        //#if UNITY_PS4
+        //        fileBase = new FileBasePS4();
+        //#endif
+        #endregion
+    }
+    public IEnumerator WriteText(string dataName, string data, string path)
+    {
+        yield return StartCoroutine(fileBase.WriteText(dataName, data, path)); // 이 코루틴이 끝날 때 까지.
+
+    }
+
+    [HideInInspector] public string readText_Result;
+
+    /// <summary>
+    /// 파일을 읽은 후, ,readText_Result에 저장합니다.
+    /// </summary>
+    /// <param name="dataName">파일의 이름을 적습니다. 예시 : "testFile.png"  </param>
+    /// <param name="dataPath">파일의 이름을 제외한 경로를 적습니다. 예시 : "Assets/data/" </param>
+    /// <returns></returns>
+    public IEnumerator ReadText(string dataName, string path)
+    {
+
+        readText_Result = string.Empty;
+        yield return StartCoroutine(fileBase.ReadText(dataName, path));
+        readText_Result = fileBase.readText_Result;
+    }
+
+    [HideInInspector] public bool isExist_Result;
+
+
+    /// <summary>
+    /// 해당 파일이 존재하는지 확인합니다. 결과는 isExist_Result에 저장됩니다.
+    /// </summary>
+    /// <param name="dataName">파일의 이름을 적습니다. 예시 : "testFile.png"  </param>
+    /// <param name="dataPath">파일의 이름을 제외한 경로를 적습니다. 예시 : "Assets/data/" </param>
+    /// <returns></returns>
+    public IEnumerator IsExist(string dataName, string dataPath)
+    {
+        yield return StartCoroutine(fileBase.IsExist(dataName, dataPath));
+        isExist_Result = fileBase.isExist_Result;
+    }
+}
