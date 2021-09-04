1d71fa77268fe6e7efb5dc3acb20638fbd7866aa
JiwonHan <acsang12@gmail.com>
2021-09-04 19:09:51 +0900

    UITitleScreen 추가

---
+++ Assets/Game/02.Scripts/UI/UITitleScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 타이틀 화면을 담당한다-!! 
+/// </summary>
+public class UITitleScreen : MonoBehaviour
+{
+
+}
