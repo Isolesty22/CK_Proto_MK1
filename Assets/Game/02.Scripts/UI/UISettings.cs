e7ba8066347e75bed766894dea1275715d97a8f3
JiwonHan <acsang12@gmail.com>
2021-09-05 21:03:27 +0900

    음량조절 작업 중

--- Assets/Game/02.Scripts/UI/UISettings.cs
+++ Assets/Game/02.Scripts/UI/UISettings.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 /// <summary>
 /// 환경설정 UI
 /// </summary>
 public class UISettings : UIBase
 {
+    public AudioMixer audioMixer;
 
     [Tooltip("현재 저장되어있는 데이터. \nDataManager의 Data_Settings와 동일해야합니다.")]
     private Data_Settings data_saved;
 
     [Tooltip("현재 수정 중인 데이터.")]
     private Data_Settings data_current;
+
+    [System.Serializable]
     public class Sliders
     {
         public Slider master;
@@ -59,6 +63,8 @@ public class UISettings : UIBase
 
 
 
+
+
     public void Button_Close(UIBase _uiBase)
     {
         //변경사항이 있다면
@@ -72,8 +78,61 @@ public class UISettings : UIBase
         }
     }
 
+    public void Button_Save()
+    {
+
+    }
+    public void ValueChanged_MasterSlider()
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log(Mathf.Lerp(0.001f, 1, VolumeSlider.master.value)) * 20);
+    }
+
+
+    /// <summary>
+    /// UI들의 값을 _data에 있는 것으로 변경합니다.
+    /// </summary>
+    /// <param name="_data"></param>
+    public void UpdateUI(Data_Settings _data)
+    {
+        VolumeSlider.master.value = GetFloat(_data.volume_master);
+        VolumeSlider.bgm.value = GetFloat(_data.volume_bgm);
+        VolumeSlider.sfx.value = GetFloat(_data.volume_sfx);
+    }
+
+    /// <summary>
+    /// _data를 실제 설정에 반영합니다.
+    /// </summary>
+    /// <param name="_data"></param>
+    public void UpdateSettings(Data_Settings _data)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log(Mathf.Lerp(0.001f, 1, GetFloat(_data.volume_master)) * 20));
+        audioMixer.SetFloat("SfxVolume", Mathf.Log(Mathf.Lerp(0.001f, 1, GetFloat(_data.volume_bgm)) * 20));
+        audioMixer.SetFloat("BgmVolume", Mathf.Log(Mathf.Lerp(0.001f, 1, GetFloat(_data.volume_sfx)) * 20));
+    }
+
+    public void ValueChanged_BGMSlider()
+    {
+        audioMixer.SetFloat("BgmVolume", Mathf.Log(Mathf.Lerp(0.001f, 1, VolumeSlider.master.value)) * 20);
+    }
+    public void ValueChanged_SFXSlider()
+    {
+        audioMixer.SetFloat("SfxVolume", Mathf.Log(Mathf.Lerp(0.001f, 1, VolumeSlider.master.value)) * 20);
+    }
+
+
+    /// <summary>
+    /// string을 float로 변환해줍니다.
+    /// </summary>
+
+    private float GetFloat(string input)
+    {
+        return (float)System.Convert.ToDouble(input);
+    }
+
     public override void RegisterUIManager()
     {
         base.RegisterUIManager();
     }
+
+
 }
