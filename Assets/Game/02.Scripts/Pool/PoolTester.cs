feb08bc62ce0c8b7a49b33b22724643c36703394
jiwon0921 <acsang12@gmail.com>
2021-09-14 15:55:12 +0900

    테스트용 코드 추가

--- Assets/Game/02.Scripts/Pool/PoolTester.cs
+++ Assets/Game/02.Scripts/Pool/PoolTester.cs
@@ -32,7 +32,7 @@ public class PoolTester : MonoBehaviour
 
     private IEnumerator Start_GameObjectPool()
     {
-        //밑작업
+        //사용 전 초기화
         poolManager.Init_Transforms();
         poolManager.Init_PoolObjectDictionary();
         poolManager.Init_Queue();
@@ -40,14 +40,23 @@ public class PoolTester : MonoBehaviour
 
         for (int i = 0; i < 3; i++)
         {
-            poolManager.SpawnThis("Arrow", Vector3.zero, Vector3.zero, null).GetComponent<TestArrow>().Shot();
+            poolManager.SpawnThis("Arrow", Vector3.zero, Vector3.zero, null);
 
             yield return new WaitForSecondsRealtime(0.2f);
         }
 
         yield return new WaitForSecondsRealtime(5f);
         Debug.Log("내용물을 지웠습니다.");
+
+        //풀 클리어
         poolManager.ClearPool();
+        //하이어라키에서 지정해놓은 리스트들과, 그걸 바탕으로 만든 딕셔너리 등은 지워지지 않습니다.
+
+        //Clear 후 재사용
+        //poolManager.Init_Transforms();
+        //poolManager.Init_Queue();
+        //poolManager.InstantiatePoolObjects();
+        //Init_PoolObjectDictionary는 하지 않아도 무방
 
     }
 
@@ -62,7 +71,7 @@ public class PoolTester : MonoBehaviour
         //cPoolManager.CreateCustomPool<TestBullet>();
 
         //int count = cPoolManager.GetPool<TestArrow>().count;
-        //for (int i = 0; i < count+1; i++)
+        //for (int i = 0; i < count + 1; i++)
         //{
         //    cPoolManager.GetPool<TestArrow>().SpawnThis(Vector3.zero, Vector3.zero, null).Shot();
         //    yield return new WaitForSecondsRealtime(0.2f);
@@ -80,7 +89,17 @@ public class PoolTester : MonoBehaviour
 
         yield return new WaitForSecondsRealtime(5f);
         Debug.Log("내용물을 지웠습니다.");
+
+        //풀 클리어
         cPoolManager.ClearPool();
+        //하이어라키에서 지정해놓은 리스트들과, 그걸 바탕으로 만든 딕셔너리 등은 지워지지 않습니다.
+
+        //Clear 후 재사용
+        //cPoolManager.CreateCustomPool<TestArrow>();
+        //cPoolManager.CreateCustomPool<TestBullet>();
+        //arrowPool = cPoolManager.CreateCustomPool<TestArrow>();
+        //bulletPool = cPoolManager.CreateCustomPool<TestBullet>();
+        //사용할 때랑 똑같이 하면 됨
     }
 
 }
