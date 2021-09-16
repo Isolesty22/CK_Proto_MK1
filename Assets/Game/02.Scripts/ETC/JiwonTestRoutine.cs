1066c58e0c8f2fe5b4cc9baa9430466df2adb956
jiwon0921 <acsang12@gmail.com>
2021-09-16 22:11:25 +0900

    키 세팅 준비작업 - Data_KeySetting 추가,KeyInputDetector 추가

--- Assets/Game/02.Scripts/ETC/JiwonTestRoutine.cs
+++ Assets/Game/02.Scripts/ETC/JiwonTestRoutine.cs
@@ -2,8 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+using System;
+//using UnityEngine.Events;
+
 public class JiwonTestRoutine : MonoBehaviour
 {
+    public KeyInputDetector keyInputDetector;
+    public Text text;
 
+    public KeyCode changedKey;
+
+    private void Start()
+    {
+        changedKey = keyInputDetector.GetKeyCode("Space");
+        text.text = changedKey.ToString();
+    }
+    public void Button_DoDetecting()
+    {
+        keyInputDetector.StartDetect();
+        StartCoroutine(ProcessKeyChange());
+    }
+
+    private IEnumerator ProcessKeyChange()
+    {
+        while (true)
+        {
+            if (!keyInputDetector.isDetecting)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+
+        //키 감지가 끝나면
+        changedKey = keyInputDetector.currentKeyCode;
+        text.text = changedKey.ToString();
+        yield break;
+        
+    }
 
 }
