56102d8d445a8a98d124adfaf3304868a1e67d81
jiwon0921 <acsang12@gmail.com>
2021-09-18 23:47:26 +0900

    KeyOption 클래스에 생성자, 복사, equals 함수 추가

--- Assets/Game/02.Scripts/KeyOption.cs
+++ Assets/Game/02.Scripts/KeyOption.cs
@@ -12,4 +12,48 @@ public class KeyOption
     public KeyCode attack = KeyCode.Z;
     public KeyCode jump = KeyCode.X;
     public KeyCode counter = KeyCode.C;
+
+    public KeyOption(KeyOption _data)
+    {
+        this.moveRight = _data.moveRight;
+        this.moveLeft = _data.moveLeft;
+        this.crouch = _data.crouch;
+        this.lookUp = _data.lookUp;
+        this.attack = _data.attack;
+        this.jump = _data.jump;
+        this.counter = _data.counter;
+    }
+
+    public KeyOption()
+    {
+        moveRight = KeyCode.RightArrow;
+        moveLeft = KeyCode.LeftArrow;
+        crouch = KeyCode.DownArrow;
+        lookUp = KeyCode.UpArrow;
+        attack = KeyCode.Z;
+        jump = KeyCode.X;
+        counter = KeyCode.C;
+    }
+
+    public void CopyData(KeyOption _data)
+    {
+        this.moveRight = _data.moveRight;
+        this.moveLeft = _data.moveLeft;
+        this.crouch = _data.crouch;
+        this.lookUp = _data.lookUp;
+        this.attack = _data.attack;
+        this.jump = _data.jump;
+        this.counter = _data.counter;
+    }
+
+    public bool IsEquals(KeyOption _data)
+    {
+        return moveRight == _data.moveRight &&
+               moveLeft == _data.moveLeft &&
+               crouch == _data.crouch &&
+               lookUp == _data.lookUp &&
+               attack == _data.attack &&
+               jump == _data.jump &&
+               counter == _data.counter;
+    }
 }
