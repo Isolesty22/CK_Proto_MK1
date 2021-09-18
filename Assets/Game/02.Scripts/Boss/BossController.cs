2f39bf07818daaa0e85ae7d5e3ccfba8b2f9cb79
jiwon0921 <acsang12@gmail.com>
2021-09-18 21:33:10 +0900

    보스 컨트롤러, 보스스테이트 추가

---
+++ Assets/Game/02.Scripts/Boss/BossController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public Animator animator;
+
+    public List<BossState> bossStateList = new List<BossState>();
+
+}
---
+++ Assets/Game/02.Scripts/Boss/BossState.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossState
+{
+    /// <summary>
+    /// 상태가 시작할때 호출됨
+    /// </summary>
+    public virtual void OnEnter()
+    {
+
+    }
+
+    /// <summary>
+    /// Monobehaviour Update에서 지속적으로 호출됨
+    /// </summary>
+    public virtual void OnUpdate()
+    {
+
+    }
+    /// <summary>
+    /// Monobehaviour FixedUpdate에서 지속적으로 호출됨
+    /// </summary>
+    public virtual void OnFixedUpdate()
+    {
+
+    }
+    /// <summary>
+    /// 상태가 끝나고 다른 상태로 진입할때 호출됨
+    /// </summary>
+    public virtual void OnExit()
+    {
+
+    }
+}
+
+
