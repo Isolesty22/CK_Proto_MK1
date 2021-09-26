b963e839f073360ab23c4b33cb7b7b4af8a1ca15
jiwon0921 <acsang12@gmail.com>
2021-09-26 19:24:08 +0900

    곰 보스 맵 속성 기초 작업, ReadOnlyAttribute&Drawer 추가, 약간의 폴더 정리

---
+++ Assets/Game/02.Scripts/Boss/Bear/BearMapInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BearMapInfo :  MonoBehaviour
+{
+    [ReadOnly]
+    public BearBlock[] bearBlocks = new BearBlock[5];
+}
+
+
+[System.Serializable]
+public class BearBlock
+{
+    [System.Serializable]
+    public class Position
+    {
+        public Position() { }
+
+        public Position(Vector3 _min, Vector3 _max)
+        {
+            max = _max;
+            min = _min;
+        }
+
+        public Vector3 max;
+        public Vector3 min;
+    }
+
+    public Position position = null;
+}
---
+++ Assets/Game/02.Scripts/ETC/ReadOnlyAttribute.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 인스펙터에서 필드를 '읽기 전용'으로 표시하여 값을 수정할 수 없습니다. 
+/// 이 속성을 사용하면 다른 CustomPropertyDrawers가 작동하지 않습니다.
+/// </summary>
+/// <seealso cref="BeginReadOnlyGroupAttribute"/>
+/// <seealso cref="EndReadOnlyGroupAttribute"/>
+public class ReadOnlyAttribute : PropertyAttribute { }
+
+/// <summary>
+/// 하나 이상의 필드를 인스펙터에서 '읽기 전용'으로 표시하여 값을 수정할 수 없습니다.
+/// <see cref="EndReadOnlyGroupAttribute"/>와 함께 사용하여 그룹을 닫을 수 있습니다.
+/// 이 속성을 사용하면 CustomPropertyDrawers를 정상적으로 작동할 수 있습니다.
+/// </summary>
+/// <seealso cref="EndReadOnlyGroupAttribute"/>
+/// <seealso cref="ReadOnlyAttribute"/>
+public class BeginReadOnlyGroupAttribute : PropertyAttribute { }
+
+/// <summary>
+/// <see cref="BeginReadOnlyGroupAttribute"/>와 함께 사용합니다.
+/// '읽기 전용' 그룹을 닫을 수 있습니다.
+/// </summary>
+/// <seealso cref="BeginReadOnlyGroupAttribute"/>
+/// <seealso cref="ReadOnlyAttribute"/>
+public class EndReadOnlyGroupAttribute : PropertyAttribute { }
