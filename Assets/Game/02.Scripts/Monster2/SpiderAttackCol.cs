20f1b7e540eeaca348bcf82e03ce080793bc2559
ruysnell <zxc1612@naver.com>
2021-09-26 17:43:52 +0900

    Spider 구현

---
+++ Assets/Game/02.Scripts/Monster2/SpiderAttackCol.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAttackCol : MonsterAttackCol
+{
+    SpiderController spiderController;
+
+    private void Start()
+    {
+        spiderController = monsterController.GetComponent<SpiderController>();
+    }
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.CompareTag("Player"))
+        {
+            spiderController.Stat2.isPlayerInCol = true;
+        }
+    }
+
+    public override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+        if (other.gameObject.transform.CompareTag("Player"))
+        {
+            spiderController.Stat2.isPlayerInCol = false;
+        }
+    }
+}
--- Assets/Game/02.Scripts/Monster2/SpiderController.cs
+++ Assets/Game/02.Scripts/Monster2/SpiderController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using DG.Tweening;
 
 public class SpiderController : MonsterController
 {
@@ -9,16 +10,19 @@ public class SpiderController : MonsterController
     [Serializable]
     public class SpiderStatus
     {
-        public float upDownSpeed;
+        public float downTime;
+        public float upTime;
         public float upDownRange;
 
-        //[Header("Sub Status")]
+        [Header("Sub Status")]
+        public bool isPlayerInCol;
+        [HideInInspector] public Vector3 upPos; 
+        [HideInInspector] public Vector3 downPos;
     }
 
     [Serializable]
     public class SpiderComponents
     {
-        public CapsuleCollider capsuleCollider;
     }
 
     [SerializeField] private SpiderStatus spiderStatus = new SpiderStatus();
@@ -27,10 +31,16 @@ public class SpiderController : MonsterController
     public SpiderStatus Stat2 => spiderStatus;
     public SpiderComponents Com2 => spiderComponents;
 
+    public Tween tween;
+    private bool moveTrigger;
     #endregion
     public override void Initialize()
     {
         base.Initialize();
+        Stat2.isPlayerInCol = false;
+        Stat2.upPos = transform.position;
+        Stat2.downPos = transform.position + Vector3.down * Stat2.upDownRange;
+        moveTrigger = true;
     }
 
     public override void Awake()
@@ -56,7 +66,22 @@ public class SpiderController : MonsterController
     protected override void Idle()
     {
         base.Idle();
-        ChangeState(MonsterState.MOVE);
+
+        if(transform.position == Stat2.upPos)
+        {
+            if (Stat2.isPlayerInCol)
+            {
+                moveTrigger = true;
+                ChangeState(MonsterState.ATTACK);
+            }
+        }
+
+        else if(transform.position == Stat2.downPos)
+        {
+            Utility.KillTween(tween);
+            tween = transform.DOMove(Stat2.upPos, Stat2.upTime).SetEase(Ease.InOutCubic);
+            tween.Play();
+        }
     }
 
     protected override void Move()
@@ -67,16 +92,22 @@ public class SpiderController : MonsterController
     protected override void Detect()
     {
         base.Detect();
-        ChangeState(MonsterState.ATTACK);
     }
     protected override void Attack()
     {
         base.Attack();
-    }
+        if (moveTrigger)
+        {
+            Utility.KillTween(tween);
+            tween = transform.DOMove(Stat2.downPos, Stat2.downTime).SetEase(Ease.InOutCubic);
+            tween.Play();
+            moveTrigger = false;
+        }
 
-    private IEnumerator UpDown()
-    {
-        yield return null;
+        if(transform.position == Stat2.downPos)
+        {
+            ChangeState(MonsterState.IDLE);
+        }
     }
 
     public override void Hit(int damage)
