17e3c89aa39d60e3f8427a5d76b07b51338b8abe
ruysnell <zxc1612@naver.com>
2021-10-03 21:45:26 +0900

    Roller 수정

---
+++ Assets/Game/02.Scripts/Monster2/Collider/RollerInRollingCol.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollerInRollingCol : MonoBehaviour
+{
+    public RollerController rollerController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Monster"))
+        {
+            other.transform.GetComponent<MonsterController>().Hit(rollerController.Stat2.rollingDamage);
+        }
+
+        else if (other.transform.name == "PlayerHitBox")
+        {
+            if (GameManager.instance.playerController.State.canParry == true)
+            {
+                if (rollerController.moveDir.x < 0)
+                {
+                    rollerController.moveDir = new Vector3(1, 0, 0);
+                    transform.localEulerAngles = new Vector3(0, 180, 0);
+                }
+                else
+                {
+                    rollerController.moveDir = new Vector3(-1, 0, 0);
+                    transform.localEulerAngles = Vector3.zero;
+                }
+            }
+        }
+    }
+}
