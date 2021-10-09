6305b73777e3b2398529eb2242fa3741bc2ce9a9
jiwon0921 <acsang12@gmail.com>
2021-10-09 17:59:14 +0900

    게임매니저에 예외처리 추가

--- Assets/Game/02.Scripts/Manager/GameManager.cs
+++ Assets/Game/02.Scripts/Manager/GameManager.cs
@@ -9,6 +9,24 @@ public class GameManager : MonoBehaviour
     public CameraManager cameraManager;
     public PlayerController playerController;
 
+    private void OnLevelWasLoaded(int level)
+    {
+        if (instance == null || instance != this)
+        {
+            instance = this;
+            //   GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+
+        if (cameraManager == null)
+        {
+            cameraManager = FindObjectOfType<CameraManager>();
+        }
+
+        if (playerController == null)
+        {
+            playerController = FindObjectOfType<PlayerController>();   
+        }
+    }
     private void Awake()
     {
         if (instance == null || instance != this)
