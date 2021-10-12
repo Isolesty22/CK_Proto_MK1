16fa19c3d51f2d52b60e53f6b8b755d5d69a8e04
jiwon0921 <acsang12@gmail.com>
2021-10-12 12:01:59 +0900

    애니메이션 복사

---
+++ Assets/Game/02.Scripts/Boss/AnimationTriggerHelper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTriggerHelper : MonoBehaviour
+{
+
+    BearController controller;
+
+    public void SkillAction()
+    {
+        controller.SkillAction();
+    }
+}
