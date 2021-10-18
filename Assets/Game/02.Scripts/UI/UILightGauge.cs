fcbd332b21cd308a66f1007b61c243a6b21d120a
한지원 <acsang12@gmail.com>
2021-10-19 07:01:39 +0900

    픽시게이지 UI 적용

--- Assets/Game/02.Scripts/UI/UILightGauge.cs
+++ Assets/Game/02.Scripts/UI/UILightGauge.cs
@@ -6,20 +6,24 @@ using UnityEngine.UI;
 public class UILightGauge : MonoBehaviour
 {
     public Slider lightGaugeSlider;
-
+    public PlayerController playerController;
+    private void Start()
+    {
+        playerController = GameManager.instance.playerController;
+    }
     private void Update()
     {
-        //if (lightGaugeSlider.value == playerCotnorller..as.w.awe.g)
-        //{
-        //lightGaugeSlider.value = playerCoaweflaewgaweopgkewa
-        //}
+        if (lightGaugeSlider.value != playerController.Stat.pixyEnerge)
+        {
+            lightGaugeSlider.value = playerController.Stat.pixyEnerge;
+        }
 
     }
     public void OnValueChanged()
     {
-        //if (lightGaugeSlider.value == playerCotnorller..as.w.awe.g)
-        //{
-        //playerCoaweflaewgaweopgkewa== lightGaugeSlider.value;
-        //}
+        if (lightGaugeSlider.value != playerController.Stat.pixyEnerge)
+        {
+            playerController.Stat.pixyEnerge = lightGaugeSlider.value;
+        }
     }
 }
