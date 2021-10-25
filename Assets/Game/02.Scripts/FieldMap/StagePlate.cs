cf21f53229afcc1104e05db269982f9c42421da9
한지원 <acsang12@gmail.com>
2021-10-25 14:34:30 +0900

    필드맵 스테이지 이동 작업 중

--- Assets/Game/02.Scripts/FieldMap/StagePlate.cs
+++ Assets/Game/02.Scripts/FieldMap/StagePlate.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 스테이지를 나타내는 발판입니다.
+/// 스테이지의 위치 정보등을 가지고 있는 발판입니다.
 /// </summary>
 public class StagePlate : MonoBehaviour
 {
-    [Header("이동할 위치")]
-    public Transform stageTransform;
+    public Transform myTransform;
 
     [Header("스프라이트 렌더러")]
     public SpriteRenderer spriteRenderer;
-    /// <summary>
-    /// 스테이지를 못가게 막습
-    /// </summary>
-    /// <param name="_value"></param>
+
     public void SetStageGrayScale(float _value)
     {
         spriteRenderer.material.SetFloat("_Greyscale", _value);
+    }
+
+    public Vector3 GetPosition()
+    {
+        return myTransform.position;
     }
 }
--- Assets/Game/02.Scripts/FieldMap/StageSelector.cs
+++ Assets/Game/02.Scripts/FieldMap/StageSelector.cs
@@ -1,24 +1,66 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 [RequireComponent((typeof(Rigidbody)))]
 /// <summary>
-/// ½ºÅ×ÀÌÁö¸¦ ¼±ÅÃÇÏ´Â, ÀÌµ¿ÇÒ ¼ö ÀÖ´Â ¹«¾ğ°¡
+/// ìŠ¤í…Œì´ì§€ë¥¼ ì„ íƒí•˜ëŠ”, ì´ë™í•  ìˆ˜ ìˆëŠ” ë¬´ì–¸ê°€
 /// </summary>
 public class StageSelector : MonoBehaviour
 {
+
+
+    public enum eState
+    {
+        Wait,
+        Move
+    }
+
     [System.Serializable]
     public class Components
     {
         public Rigidbody rigidBody;
         public Animator animator;
     }
-
     public Components Com => _components;
 
     [SerializeField]
     private Components _components;
+
+    [HideInInspector]
+    public float timer = 0f;
+    [HideInInspector]
+    public float progress = 0f;
+    [HideInInspector]
+    public float moveTime = 3f;
+
+    public eState state;
+
+    private Vector3 currentPosition;
+    private Vector3 destPosition;
+
+    private IEnumerator ProcessMove()
+    {
+        state = eState.Move;
+
+        while (progress < 1f)
+        {
+            timer += Time.deltaTime;
+            progress = timer / moveTime;
+
+            currentPosition = Vector3.Lerp(Com.rigidBody.position, destPosition, progress);
+
+            SetPosition(currentPosition);
+
+            yield return YieldInstructionCache.WaitForFixedUpdate;
+        }
+        Debug.Log("Finish Move!");
+
+        state = eState.Wait;
+    }
+
+    public void SetDestination(Vector3 position) => destPosition = position;
+    public void SetPosition(Vector3 position) => Com.rigidBody.position = position;
 
 }
