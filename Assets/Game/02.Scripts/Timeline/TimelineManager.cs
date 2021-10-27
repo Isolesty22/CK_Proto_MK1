3a6cbef143992fb9fa0d7d7629109b0e569d3e7d
한지원 <acsang12@gmail.com>
2021-10-27 14:05:50 +0900

    타임라인매니저 작동 확인

--- Assets/Game/02.Scripts/Timeline/TimelineManager.cs
+++ Assets/Game/02.Scripts/Timeline/TimelineManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using System;
 
 public class TimelineManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayableDirector director;
+
+    public Action OnTimelineEnded;
+    private void Awake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+    }
+
+    /// <summary>
+    /// 타임라인 재생이 끝날 때 까지 기다립니다.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator WaitTimelineEnd()
+    {
+        yield return new WaitUntil(() => director.state != PlayState.Playing);
+        Debug.LogWarning("[TimelineManager] End Timeline : " + director.name + "." + director.playableAsset.name);
+        OnTimelineEnded.Invoke();
+    }
+
+    public void SetTimeline(TimelineAsset _timeline)
+    {
+        director.playableAsset = _timeline;
+    }
+    public void Play()
+    {
+        director.Play();
+        StartCoroutine(WaitTimelineEnd());
     }
 }
