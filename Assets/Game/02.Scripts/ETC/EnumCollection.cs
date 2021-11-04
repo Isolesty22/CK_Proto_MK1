df08aa2b0beab6b27435884b30849ce4a3a71170
한지원 <acsang12@gmail.com>
2021-11-05 01:41:07 +0900

    TagName에 Boss 추가

--- Assets/Game/02.Scripts/ETC/EnumCollection.cs
+++ Assets/Game/02.Scripts/ETC/EnumCollection.cs
@@ -260,9 +260,10 @@ public static class TagName
     public static readonly string Arrow = "Arrow";
     public static readonly string ParryingObject = "ParryingObject";
     public static readonly string Monster = "Monster";
-   // public static readonly string  = "FieldMap";
+    public static readonly string Boss = "Boss";
+    // public static readonly string  = "FieldMap";
 }
-    public class EnumCollection : MonoBehaviour
+public class EnumCollection : MonoBehaviour
 {
 
 }
