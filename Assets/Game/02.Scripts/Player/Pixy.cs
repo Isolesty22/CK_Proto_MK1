91e49c66e58dacc170ab434508a4baaa1feaaf5c
isolesty23 <isolesty0814@gmail.com>
2021-11-15 00:26:55 +0900

    pixy

--- Assets/Game/02.Scripts/Player/Pixy.cs
+++ Assets/Game/02.Scripts/Player/Pixy.cs
@@ -47,8 +47,6 @@ public class Pixy : MonoBehaviour
         firePos = pc.Com.pixyFirePos;
         ultPos = pc.Com.pixyUltPos;
 
-        getPixy = true;
-
         if(getPixy)
         {
             transform.position = targetPos;
