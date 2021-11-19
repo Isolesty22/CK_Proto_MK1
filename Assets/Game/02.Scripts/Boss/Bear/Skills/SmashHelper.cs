6b2363a4634140ba9b3f0ce7750fb59690657fec
한지원 <acsang12@gmail.com>
2021-11-19 20:12:38 +0900

    쪼개진 돌 이동 제대로 작동하는거 확인

--- Assets/Game/02.Scripts/Boss/Bear/Skills/SmashHelper.cs
+++ Assets/Game/02.Scripts/Boss/Bear/Skills/SmashHelper.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 곰의 스매시 스킬에 사용되는 돌을 관리합니다.
+/// </summary>
 public class SmashHelper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform myTransform;
+    public SmashRock[] smashRocks;
+
+
+    public Transform[] transforms;
+    private void Awake()
     {
-        
+        for (int i = 0; i < smashRocks.Length; i++)
+        {
+            smashRocks[i].UpdateVectors(transforms[0].position, transforms[1].position, transforms[2].position);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Start()
     {
-        
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < smashRocks.Length; i++)
+        {
+            smashRocks[i].StartMove();
+            yield return new WaitForSeconds(0.3f);
+        }
+
     }
 }
