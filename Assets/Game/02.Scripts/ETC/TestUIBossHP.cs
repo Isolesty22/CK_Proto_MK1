46439315fb538a962fe6f3c20ef8a220fbb86a84
한지원 <acsang12@gmail.com>
2021-11-19 17:27:05 +0900

    보스 체력 UI 페이드 연출 추가

--- Assets/Game/02.Scripts/ETC/TestUIBossHP.cs
+++ Assets/Game/02.Scripts/ETC/TestUIBossHP.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TestUIBossHP : MonoBehaviour
+public class TestUIBossHP : UIBase
 {
     public BossController bossController;
     public Image hpImage;
     private float maxHP;
-    void Start()
+
+
+    private void Awake()
     {
+        Init();
+    }
+    public override void Init()
+    {
+        CheckOpen();
+
+        Com.canvas.enabled = false;
+
         string sceneName = SceneManager.GetActiveScene().name;
 
         if (sceneName == SceneNames.stage_02 || sceneName == SceneNames.stage_04)
@@ -18,11 +28,22 @@ public class TestUIBossHP : MonoBehaviour
             bossController = FindObjectOfType<BossController>();
             maxHP = bossController.hp;
         }
-        else
+    }
+    void Start()
+    {
+        GameManager.instance.timelineManager.onTimelineEnded += OnTimelineEnded;
+    }
+
+    public void OnTimelineEnded()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == SceneNames.stage_02 || sceneName == SceneNames.stage_04)
         {
-            gameObject.SetActive(false);
+            StartCoroutine(ProcessOpen());
         }
     }
+
+
 
     private void Update()
     {
