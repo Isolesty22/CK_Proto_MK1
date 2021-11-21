367f91abfef1d21bad666cf80fb339ca697e82c4
한지원 <acsang12@gmail.com>
2021-11-22 00:54:14 +0900

    포탈 버그 수정 중 / 영상 2배속 버튼 추가

--- Assets/Game/02.Scripts/ETC/StagePortal.cs
+++ Assets/Game/02.Scripts/ETC/StagePortal.cs
@@ -8,7 +8,6 @@ using UnityEngine;
 public class StagePortal : MonoBehaviour
 {
     public Collider col;
-    private PlayerController player;
 
     [Tooltip("true일 경우, 2초간 걸어간 뒤 스테이지를 이동합니다.")]
     public bool moveOnEnter;
@@ -32,10 +31,6 @@ public class StagePortal : MonoBehaviour
         gameObject.SetActive(true);
     }
 
-    private void Start()
-    {
-        player = GameManager.instance.playerController;
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(TagName.Player))
@@ -57,9 +52,9 @@ public class StagePortal : MonoBehaviour
     private IEnumerator CoStageClear()
     {
 
-        player.State.isCrouching = false;
-        player.State.isLookUp = false;
-        player.State.isAttack = false;
+        GameManager.instance.playerController.State.isCrouching = false;
+        GameManager.instance.playerController.State.isLookUp = false;
+        GameManager.instance.playerController.State.isAttack = false;
 
         //UI 끄기
         UIPlayerHP ui = UIManager.Instance.GetUI("UIPlayerHP") as UIPlayerHP;
@@ -67,8 +62,8 @@ public class StagePortal : MonoBehaviour
 
         //오른쪽으로 이동
         GameManager.instance.cameraManager.vcam.Follow = null;
-        player.State.moveSystem = true;
-        player.InputVal.movementInput = 1f;
+        GameManager.instance.playerController.State.moveSystem = true;
+        GameManager.instance.playerController.InputVal.movementInput = 1f;
         yield return new WaitForSeconds(2f);
         GameManager.instance.StageClear();
     }
--- Assets/Game/02.Scripts/UI/UIMovieScreen.cs
+++ Assets/Game/02.Scripts/UI/UIMovieScreen.cs
@@ -16,14 +16,22 @@ public class UIMovieScreen : UIBase
 
     [Tooltip("영상이 끝나면 해당 패널이 페이드 됩니다.")]
     public Image blackPanel;
+    [Space(5)]
+    [Tooltip("플레이 스피드를 조절하는 버튼")]
+    public Button playbackSpeedButton;
+
+    [Serializable]
+    public class PlaybackSpriteSet
+    {
+        public Sprite defaultSprite;
+        public SpriteState sprites;
+    }
+    public PlaybackSpriteSet x1SpriteSet;
+    public PlaybackSpriteSet x2SpriteSet;
 
     [Space(5)]
     public VideoClip[] videoClips;
 
-    private bool isSkip = false;
-
-    [Tooltip("true일 경우, 영상 플레이가 끝날 때 렌더텍스처를 해제하지 않습니다.")]
-    private bool doNotRelease;
 
     //Black Panel Colors
     private Color startColor = new Color(0, 0, 0, 0);
@@ -32,16 +40,26 @@ public class UIMovieScreen : UIBase
     public IEnumerator playMovie;
     public event Action onMovieEnded = null;
 
+    private bool isSkip = false;
+
+    [Tooltip("true일 경우, 영상 플레이가 끝날 때 렌더텍스처를 해제하지 않습니다.")]
+    private bool doNotRelease;
+
+    private bool isPlaybackSpeedUp;
+
     private void Awake()
     {
         playMovie = CoPlayMovie();
         isSkip = false;
+        isPlaybackSpeedUp = false;
+        videoPlayer.playbackSpeed = 1f;
         //OnMovieEnded +=  delegate{ Close(); };
     }
     private void Start()
     {
         Init();
         UIManager.Instance.AddDict(this);
+
     }
     public override void Init()
     {
@@ -60,12 +78,37 @@ public class UIMovieScreen : UIBase
         StartCoroutine(ProcessClose());
         return true;
     }
+    /// <summary>
+    /// 아무것도 안하고 재생만 합니다.
+    /// </summary>
     public void Play()
     {
         StartCoroutine(CoPlayMovie());
     }
 
+    public void Button_ToggleSpeed()
+    {
+        //스피드가 올라간 상태라면
+        if (isPlaybackSpeedUp)
+        {
+            videoPlayer.playbackSpeed = 1f;
+            playbackSpeedButton.image.sprite = x1SpriteSet.defaultSprite;
 
+            playbackSpeedButton.spriteState = x1SpriteSet.sprites;
+        }
+        //스피드가 안올라간 상태라면
+        else
+        {
+            videoPlayer.playbackSpeed = 2f;
+            playbackSpeedButton.image.sprite = x2SpriteSet.defaultSprite;
+            playbackSpeedButton.spriteState = x2SpriteSet.sprites;
+        }
+        isPlaybackSpeedUp = !isPlaybackSpeedUp;
+    }
+    /// <summary>
+    /// 해당 인덱스의 영상을 재생합니다.
+    /// </summary>
+    /// <param name="_index"></param>
     public void Play(int _index)
     {
         videoPlayer.clip = videoClips[_index];
