cf8f6f2c6894471c91b508fcc066a5c0f570c9f5
한지원 <acsang12@gmail.com>
2021-11-22 01:39:01 +0900

    보스 HP 일정 이상 깎이면 HP바가 더이상 깎이지 않음. 죽고나서야 다 깎임

--- Assets/Game/02.Scripts/UI/UIBossHP.cs
+++ Assets/Game/02.Scripts/UI/UIBossHP.cs
@@ -9,6 +9,7 @@ public class UIBossHP : UIBase
 
     public BossController bossController;
     public Image hpImage;
+
     private float maxHP;
     private string sceneName;
 
@@ -42,6 +43,7 @@ public class UIBossHP : UIBase
         if (sceneName == SceneNames.stage_02 || sceneName == SceneNames.stage_04)
         {
             StartCoroutine(ProcessOpen());
+            StartCoroutine(UpdateUI());
         }
     }
 
@@ -68,12 +70,39 @@ public class UIBossHP : UIBase
         gameObject.SetActive(false);
     }
 
-    private void Update()
-    {
-        timer += Time.deltaTime * 0.01f;
-        progres = Mathf.Lerp(progres, bossController.hp / maxHP, timer);
-        hpImage.fillAmount = progres;
 
-        // hpImage.fillAmount = bossController.hp / maxHP;
+    private IEnumerator UpdateUI()
+    {
+        while (hpImage.fillAmount > 0.015f)
+        {
+            timer += Time.deltaTime * 0.01f;
+            progres = Mathf.Lerp(progres, bossController.hp / maxHP, timer);
+            hpImage.fillAmount = progres;
+            yield return null;
+        }
+
+        yield return new WaitWhile(() => bossController.animator.enabled);
+        float startFillAmount = hpImage.fillAmount;
+
+        float oneSecTimer = 0f;
+
+        while (progres < 1f)
+        {
+            oneSecTimer += Time.deltaTime;
+            progres = oneSecTimer / 1f;
+            hpImage.fillAmount = Mathf.Lerp(startFillAmount, 0f, oneSecTimer);
+
+            yield return null;
+        }
+
     }
+
+    //private void Update()
+    //{
+    //    timer += Time.deltaTime * 0.01f;
+    //    progres = Mathf.Lerp(progres, bossController.hp / maxHP, timer);
+    //    hpImage.fillAmount = progres;
+
+    //    // hpImage.fillAmount = bossController.hp / maxHP;
+    //}
 }
