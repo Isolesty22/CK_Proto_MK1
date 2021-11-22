bda6708dfcb137f7331f639f97d89496fbb1b725
한지원 <acsang12@gmail.com>
2021-11-22 23:46:25 +0900

    포탈에 닿으면 골! UI가 뜸

--- Assets/Game/02.Scripts/ETC/StagePortal.cs
+++ Assets/Game/02.Scripts/ETC/StagePortal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 닿으면 게임매니저의 StageClear를 호출합니다. 
@@ -9,6 +10,11 @@ public class StagePortal : MonoBehaviour
 {
     public Collider col;
 
+    public CanvasGroup canvasGroup;
+    public Image image;
+
+
+    private RectTransform rectTransform;
     [Tooltip("true일 경우, 2초간 걸어간 뒤 스테이지를 이동합니다.")]
     public bool moveOnEnter;
 
@@ -16,9 +22,11 @@ public class StagePortal : MonoBehaviour
     public bool activeOnAwake;
     private void Awake()
     {
+        canvasGroup.alpha = 0f;
         if (activeOnAwake)
         {
             gameObject.SetActive(true);
+            rectTransform = image.rectTransform;
         }
         else
         {
@@ -40,18 +48,38 @@ public class StagePortal : MonoBehaviour
             if (moveOnEnter)
             {
                 StartCoroutine(CoStageClear());
+                StartCoroutine(CoFadeClearImage());
             }
             else
             {
                 GameManager.instance.StageClear();
-
             }
         }
     }
 
+    Vector2 leftPos = new Vector2(-2000f, 0f);
+    Vector2 endPos = new Vector2(0f, 0f);
+    private IEnumerator CoFadeClearImage()
+    {
+        rectTransform.anchoredPosition = leftPos;
+
+//canvasGroup.alpha = 0f;
+
+        float progress = 0f;
+        float timer = 0f;
+        canvasGroup.alpha = 1f;
+        while (progress < 1f)
+        {
+            timer += Time.deltaTime;
+            progress = timer / 0.5f;
+
+            rectTransform.anchoredPosition = Vector2.Lerp(leftPos, endPos, progress);
+            //canvasGroup.alpha = Mathf.Lerp(0f, 1f, progress);
+            yield return null;
+        }
+    }
     private IEnumerator CoStageClear()
     {
-
         GameManager.instance.playerController.State.isCrouching = false;
         GameManager.instance.playerController.State.isLookUp = false;
         GameManager.instance.playerController.State.isAttack = false;
