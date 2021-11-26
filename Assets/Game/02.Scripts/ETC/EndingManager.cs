fbdf2e363bd7941f4d25529ee3ebe5f51be9f14c
한지원 <acsang12@gmail.com>
2021-11-26 14:54:20 +0900

    시연용 세팅

--- Assets/Game/02.Scripts/ETC/EndingManager.cs
+++ Assets/Game/02.Scripts/ETC/EndingManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class EndingManager : MonoBehaviour
 {
     private UIMovieScreen movieScreen;
     private UIEndingCredit endingCredit;
+
+    public PlayableDirector director;
     private IEnumerator Start()
     {
         //로딩창이 사라질때까지 대기
-       yield return new WaitWhile(() => SceneChanger.Instance.isLoading);
+        yield return new WaitWhile(() => SceneChanger.Instance.isLoading);
 
         yield return null;
         //무비스크린 데려오기
@@ -30,19 +33,33 @@ public class EndingManager : MonoBehaviour
         endingCredit = UIManager.Instance.GetUI("UIEndingCredit") as UIEndingCredit;
         endingCredit.StartCoroutine(endingCredit.Up());
 
+        AudioClip tempClip = null;
+
+        if (AudioManager.Instance.clipDict_BGM.TryGetValue("Stage1BGM", out tempClip))
+        {
+            AudioManager.Instance.Audios.audioSource_BGM.clip = tempClip;
+            AudioManager.Instance.Audios.audioSource_BGM.Play();
+        }
+
+        StartCoroutine(CoStartTimeline());
+
         endingCredit.onCreditMoveEnded += EndingCredit_onCreditMoveEnded;
     }
 
+    private IEnumerator CoStartTimeline()
+    {
+        yield return new WaitForSeconds(3f);
 
+        director.Play();
+    }
     private IEnumerator OnCreditMoveEnded()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
         SceneChanger.Instance.LoadThisScene(SceneNames.mainMenu);
     }
     private void EndingCredit_onCreditMoveEnded()
     {
         endingCredit.onCreditMoveEnded -= EndingCredit_onCreditMoveEnded;
         StartCoroutine(OnCreditMoveEnded());
-
     }
 }
